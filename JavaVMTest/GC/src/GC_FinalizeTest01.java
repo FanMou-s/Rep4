ff2e3a263c80b1e01bb0b4ce549d83c6891ba61e
fanmou-ja <884712955@qq.com>
2020-09-17 12:25:37 +0800

    测试修改版02

--- JavaVMTest/GC/src/GC_FinalizeTest01.java
+++ JavaVMTest/GC/src/GC_FinalizeTest01.java
@@ -13,7 +13,7 @@ public class GC_FinalizeTest01 {
     @Override
     protected void finalize() throws Throwable {
         super.finalize();
-        System.out.println("finalize method executed! test01ppp");
+        System.out.println("finalize method executed! test02ppp");
         GC_FinalizeTest01.SAVE_HOOK=this;
     }
 
